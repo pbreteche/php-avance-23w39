78f009cabe049f6fbf97ec1cd0922af5a3dafef5
Pierre Bretéché <pbreteche@dawan.fr>
2023-09-28 10:01:59 +0200

    Démarrage projet Doctrine, ouverture de connexion

---
+++ 06-orm/src/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+// https://www.doctrine-project.org/projects/doctrine-orm/en/2.16/tutorials/getting-started.html
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Attributes
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: array(__DIR__."/src"),
+    isDevMode: true,
+);
+
+$dsnParser = new DsnParser(['mysql' => 'pdo_mysql']);
+$connectionParams = $dsnParser
+    ->parse(file_get_contents(__DIR__.'/../config/doctrine-dsn'));
+
+// configuring the database connection
+$connection = DriverManager::getConnection($connectionParams, $config);
+
+// obtaining the entity manager
+$entityManager = new EntityManager($connection, $config);
